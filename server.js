0e4d8a796cd2adf2e7e55d9037e006daca5fb572
Tadeu Oliveira <tadeu_oliveira@id.uff.br>
2021-08-18 20:14:58 -0300

    requires mongo dependencies and implements url validation

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,15 +17,34 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/is-mongoose-ok", function (req, res) {
+  if (mongoose) {
+    res.json({ isMongooseOk: !!mongoose.connection.readyState });
+  } else {
+    res.json({ isMongooseOk: false });
+  }
 });
+
+app.get('/api/shorturl/:dns', function(req, res) {
+});
+
+app.post('/api/shorturl', function(req, res){
+
+  dns.lookup(req.body.url,null,(err) => {
+    if(err){
+      res.json({error: 'invalid url', value: req.body.url});
+      return;
+    }
+    res.json({status: 'ok!', value: req.body.url});
+  });
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
